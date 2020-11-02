f95647129cdf5fb7509722f33ff3ae322f2f7281
Aleksandra Desmurs-Linczewska <desmurs.linczewska@gmail.com>
2020-11-02 22:50:53 +0100

    minor cleanup

--- xstate/lista-linii.js
+++ xstate/lista-linii.js
@@ -25,14 +25,6 @@ const invokeFetchSubreddit = async () => {
   }
 };
 
-// function invokeFetchSubreddit(context) {
-//   const {subreddit} = context;
-
-//   return fetch(`https://www.reddit.com/r/${subreddit}.json`)
-//     .then((response) => response.json())
-//     .then((json) => json.data.children.map((child) => child.data));
-// }
-
 export const redditMachine = Machine({
   id: 'reddit',
   initial: 'idle',
